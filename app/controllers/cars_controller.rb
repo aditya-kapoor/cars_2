a22ee98c47c8af50c23fdff48cb83a1af482f3fd
Aditya Kapoor <adityakapoor.mait@gmail.com>
2021-08-10 23:05:53 +0000

    Display associated collections on show pages

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -42,8 +42,14 @@ class CarsController < ApplicationController
   # DELETE /cars/1
   def destroy
     @car.destroy
-    redirect_to cars_url, notice: 'Car was successfully destroyed.'
+    message = "Car was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to cars_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
